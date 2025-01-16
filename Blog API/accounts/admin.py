cda8c5a4389baa5d5dd04144c36ad09be194b5aa
Sam <sw.waithakasamuel@gmail.com>
2025-01-16 15:48:30 +0300

    Implement login and logout functionality with DRF authentication
    - Added login and logout functionality by including "rest_framework.urls" in project-level URLs.
    - Downgraded Django, DRF, and CORS to older versions to resolve compatibility issues.
    - Ensured proper handling of authentication endpoints for user login and logout.

--- Blog API/accounts/admin.py
+++ Blog API/accounts/admin.py
@@ -10,8 +10,8 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'name', 'is_staff']
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name',)}),)
-    add_fieldsets = UserAdmin.add_fieldsets  + ((None, {'fields': ('name',)}),)
+    list_display = ['username','name', 'email', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
 
 admin.site.register(CustomUser, CustomUserAdmin)
